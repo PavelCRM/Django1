4c6270778881b7d99347923f76838993aa887815
PavelCRM <113143322+PavelCRM@users.noreply.github.com>
2023-10-23 00:18:40 +0300

    Add files via upload

--- myproject3/myproject/settings.py
+++ myproject3/myproject/settings.py
@@ -9,12 +9,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2r*kmrx_b3pr6%7-#86ww0!38o4(fnn@n+39cp_zq(^$kk2v9x'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
-ALLOWED_HOSTS = []
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+
+ALLOWED_HOSTS = ['127.0.0.1',
+'Fenriz.pythonanywhere.com',
+]
+
+STATIC_ROOT = BASE_DIR / 'static/'
 
 
 # Application definition
@@ -67,10 +75,18 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+DATABASES = DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '<Fenriz$default>',
+        'USER': '<Fenriz>',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': '<Fenriz.mysql.pythonanywhere-services.com>',
+        'OPTIONS': {
+'init_command': "SET NAMES 'utf8mb4';"
+                "SET sql_mode='STRICT_TRANS_TABLES'",
+'               charset': 'utf8mb4',
+        },
     }
 }
 
